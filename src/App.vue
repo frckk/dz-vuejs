a7711b7b9937766073c82a2cad5df0030e8d67e2
frckk <morshin55@gmail.com>
2024-08-26 09:09:31 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Card
+      title="Card title"
+      text="Some quick example text to build on the card"
+      buttonText="Go somewhere"
+    />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>

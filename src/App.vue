5c60a85be7f5641ab88d1939770f6892fff14381
frckk <morshin55@gmail.com>
2024-08-26 09:08:15 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Card title="task 1" />
+    <Card text="task 2" />
+    <Card title="task 3 title" text="task 3 text" />
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="title || text" class="card">
+    <div v-if="title || text" class="card-body">
+      <h4 v-if="title" class="card-title">{{ title }}</h4>
+      <p v-if="text" class="card-text">{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard', // Use a multi-word name???
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
